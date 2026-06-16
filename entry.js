adb3d562a724a84f992246481e934f1715e513b2
Felix <midi2001@mail.ru>
2026-06-16 14:24:37 +0300

    build: include Buffer in GramJS bundle
    Expose the Node `buffer` `Buffer` implementation through the bundled GramJS entry point so consumers can access it from the browser-compatible build.

--- entry.js
+++ entry.js
@@ -1,9 +1,11 @@
 const { TelegramClient } = require("telegram");
 const { MemorySession } = require("telegram/sessions");
 const { Api } = require("telegram/tl");
+const { Buffer } = require("buffer");
 
 module.exports = {
     TelegramClient,
     MemorySession,
-    Api
+    Api,
+    Buffer
 };
